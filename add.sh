20895dba5f7d897207e7f5a892f3ac4c1ff13372
ramesh <ramesh@gmail.com>
2025-01-08 00:17:41 +0530

    this addition example of code

---
+++ add.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo " add the two number "
+
+echo " enter the 1st number"
+
+read a
+
+echo " enter the 2nd number"
+
+read b
+
+sum=$((a+b))
+
+echo " Total = $sum"
+
+echo " code end"
